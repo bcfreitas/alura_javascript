b6fd2b7b6e933d96113f01956de24d2354be1b82
Bruno Freitas <bcfreitas@gmail.com>
2021-11-06 15:22:26 -0300

    add event listeners em botão

--- js/principal.js
+++ js/principal.js
@@ -47,3 +47,37 @@ for(var i=0;i<pacientes.length;i++){
 
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", 
+        //modo 1: chamar uma função que espera receber
+        //um objeto Event (atente que não podemos especificar os parametros).
+        //a IDE não saberá na declaração da função que o parâmetro é Event, não terá autoComplete
+        //
+           adicionar
+        //
+        //modo 2: declarar uma arrow function anônima passando o Event como parâmetro
+        //vantagem: a IDE entende que o parâmetro e é um Event, e permite autocomplete;
+        //
+        //   (e) => {e.preventDefault(); console.log("adiconar clicado!");}
+        //
+        //modo 3: declarar uma função padrão anônima que receberá como parâmetro o Event
+        //vantagem: a IDE entende que o parâmetro e é um Event, e permite autocomplete;
+        //
+        //   function(e){
+        //        e.preventDefault();
+        //        console.log("adicionar clicado");
+        //    }
+        //modo 4: declarar uma função anônima sem parâmetros e utilizar a variável event
+        //a variável event esá deprecated (proveniente da MS), pode ocorrer incompatibilidades. Usar o objeto Event em lugar.
+        //
+        //    function(){
+        //        event.preventDefault();
+        //        console.log("adicionar clicado");
+        //      }
+);
+
+function adicionar(e){
+    e.preventDefault();
+    console.log("adicionar clicado");
+}
+
