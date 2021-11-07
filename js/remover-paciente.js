5375457588ad073099649cbbb8b5655e3836096a
Bruno Freitas <bcfreitas@gmail.com>
2021-11-07 09:38:03 -0300

    animação na remoção com css e setTimeout

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,5 +3,8 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(e){
-   (e.target).parentNode.remove();
+    e.target.parentNode.classList.add("fade-out");
+    setTimeout(function(){
+        (e.target).parentNode.remove();
+    }, 500);
 });
